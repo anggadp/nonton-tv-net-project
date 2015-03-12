d1aae78b7adaafca50fe0b291b7185c086144fc1
nontontv2013 <nontontv2013@gmail.com>
2015-03-12 09:48:24 +0700

    Nonton TV Net

--- public_html/js/Apps.js
+++ public_html/js/Apps.js
@@ -908,7 +908,8 @@ $(document).ready(function(){
         });
         
     function SHOWALLCNLHM(z,o){
-       
+        $.ajax({
+            success: function(){
                 if (o < 5)
                     $('.sw-all-'+z).empty().append('');
                 $('.sw-all-'+z)
@@ -921,7 +922,8 @@ $(document).ready(function(){
                             $(this).empty().append('Show less');
                         }
                     });
-            
+            }
+        });
     };    
     
     function CNLITEMHM(o,ct){
